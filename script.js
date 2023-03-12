7e3d10ff30e28a8ff33e48dbe822c323d7a95f51
estxfanny <estefannyoliveira453@gmail.com>
2023-03-12 13:57:22 -0400

    update

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function carregar(){
     var img = document.getElementById("foto")
     var msg = document.getElementById("bloco")
     var nota = Number(prompt("Digite sua nota de 0 a 10: "))
-    if (nota >=7){
+    if (nota >=6){
         img.src = 'imagens/emojifesta22.png'
         msg.innerHTML = `Sua nota foi <strong>${nota}</strong> e você foi: <strong>APROVADO!</strong>`
         document.body.style.background = "green"
-    }else if (nota >=5 && nota <=6) {
+    }else if (nota >=5 && nota <=5.9) {
         img.src = 'imagens/emojineutro2.png'
         msg.innerHTML = `Sua nota foi <strong>${nota}</strong> e você está de <strong>recuperação!</strong>`
         document.body.style.background = "#ffcf21"
