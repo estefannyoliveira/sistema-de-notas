0a60805aff4a7ef62b94ad4e22bdc2b62d1aa8ed
estxfanny <estefannyoliveira453@gmail.com>
2023-03-12 14:06:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function carregar(){
     var nota = Number(prompt("Digite sua nota de 0 a 10: "))
     if (nota >=6){
         img.src = 'imagens/emojifesta22.png'
-        msg.innerHTML = `Sua nota foi <strong>${nota}</strong> e você foi: <strong>APROVADO!</strong>`
+        msg.innerHTML = `Sua nota foi <strong>${nota}</strong> e você está: <strong>APROVADO!</strong>`
         document.body.style.background = "green"
     }else if (nota >=5 && nota <=5.9) {
         img.src = 'imagens/emojineutro2.png'
@@ -12,7 +12,7 @@ function carregar(){
         document.body.style.background = "#ffcf21"
     } else{
         img.src = 'imagens/emojitriste2.png'
-        msg.innerHTML = `Sua nota foi <strong>${nota}</strong> e você foi: <strong>REPROVADO!</strong>`
+        msg.innerHTML = `Sua nota foi <strong>${nota}</strong> e você está: <strong>REPROVADO!</strong>`
         document.body.style.background = "red"
 
     }
