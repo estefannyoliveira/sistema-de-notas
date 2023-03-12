fd58de408f3a92b241ee3cfed8c83976b4497329
estxfanny <estefannyoliveira453@gmail.com>
2023-03-12 14:59:39 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,22 +4,22 @@ function carregar(){
 
     var n1 =  Number(prompt("Digite a 1° nota: "))
     var n2 =  Number(prompt("Digite a 2° nota: "))
-    var n3 =  Number(prompt("Digite a 2° nota: "))
+    var n3 =  Number(prompt("Digite a 3° nota: "))
 
     var nota = (n1+n2+n3)/3
     var media = nota.toFixed(2)
 
     if (media >=6){
         img.src = 'imagens/emojifesta22.png'
-        msg.innerHTML = `Sua média foi <strong>${media}</strong> e você está: <strong>APROVADO!</strong>`
+        msg.innerHTML = `Sua média é <strong>${media}</strong> e você está: <strong>APROVADO!</strong>`
         document.body.style.background = "green"
     }else if (media >=5 && media <=5.99) {
         img.src = 'imagens/emojineutro2.png'
-        msg.innerHTML = `Sua média foi <strong>${media}</strong> e você está de <strong>recuperação!</strong>`
+        msg.innerHTML = `Sua média é <strong>${media}</strong> e você está de <strong>recuperação!</strong>`
         document.body.style.background = "#ffcf21"
     } else{
         img.src = 'imagens/emojitriste2.png'
-        msg.innerHTML = `Sua média foi <strong>${media}</strong> e você está: <strong>REPROVADO!</strong>`
+        msg.innerHTML = `Sua média é <strong>${media}</strong> e você está: <strong>REPROVADO!</strong>`
         document.body.style.background = "red"
 
     }
