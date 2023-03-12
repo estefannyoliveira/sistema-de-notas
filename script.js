2eb4e06a3164793fb2ca2f3d28ac4b424704fc64
estxfanny <estefannyoliveira453@gmail.com>
2023-03-12 13:39:02 -0400

    files

---
+++ script.js
@@ -0,0 +1,19 @@
+function carregar(){
+    var img = document.getElementById("foto")
+    var msg = document.getElementById("bloco")
+    var nota = Number(prompt("Digite sua nota de 0 a 10: "))
+    if (nota >=7){
+        img.src = 'imagens/emojifesta22.png'
+        msg.innerHTML = `Sua nota foi <strong>${nota}</strong> e você foi: <strong>APROVADO!</strong>`
+        document.body.style.background = "green"
+    }else if (nota >=5 && nota <=6) {
+        img.src = 'imagens/emojineutro2.png'
+        msg.innerHTML = `Sua nota foi <strong>${nota}</strong> e você está de <strong>recuperação!</strong>`
+        document.body.style.background = "#ffcf21"
+    } else{
+        img.src = 'imagens/emojitriste2.png'
+        msg.innerHTML = `Sua nota foi <strong>${nota}</strong> e você foi: <strong>REPROVADO!</strong>`
+        document.body.style.background = "red"
+
+    }
+}
